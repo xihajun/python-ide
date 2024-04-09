344b24ca5c579c84b02f06b40442d31f480f6cd0
xihajun <mw18386@my.bristol.ac.uk>
2024-04-09 23:22:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import uuid
 
+# Function to execute the Python code
 def execute_python_code(code):
     # Create a unique filename
     unique_filename = f"code_{uuid.uuid4().hex}.py"
@@ -12,41 +13,33 @@ def execute_python_code(code):
     # Execute the code and capture the output
     try:
         output = subprocess.check_output(
-            ["python", unique_filename], stderr=subprocess.STDOUT, timeout=10
+            ["python", unique_filename], stderr=subprocess.STDOUT, timeout=5
         )
         return output.decode(), False
     except subprocess.CalledProcessError as e:
         return e.output.decode(), True
-    except subprocess.TimeoutExpired:
-        return "Error: Execution time exceeded limit.", True
     finally:
         # Make sure to remove the file after execution
         os.remove(unique_filename)
 
+# Streamlit layout configuration to hide the hamburger menu and footer
+st.set_page_config(page_title='Online Python IDE', layout='wide', initial_sidebar_state="collapsed", menu_items={'Get Help': None, 'Report a bug': None, 'About': None})
+
+# Run button at the top
+run_code = st.button('Run Code')
+
+# Columns for the code input and output display
 col1, col2 = st.columns(2)
 
 with col1:
-    code = st.text_area("", height=300, key="code")
+    code = st.text_area("", "Write your code here...", height=390)
 
 with col2:
-    output_area = st.empty()  # Placeholder for output
+    output_placeholder = st.empty()  # Placeholder for output
 
-# Sidebar for run button
-with st.sidebar:
-    run_code = st.button('Run Code')
-
-# Execute the code when the button is clicked
-if run_code:
-    if code.strip() != "":
-        with output_area:
-            with st.spinner('Running...'):
-                output, error = execute_python_code(code)
-            st.code(output, language='python')
-            # Highlight if there's an error
-            if error:
-                st.error('Error in execution. See output for details.')
-    else:
-        st.sidebar.warning('Please enter some code to execute.')
-
-# Instructions on the sidebar
-st.sidebar.info('Enter Python code in the left column and press "Run Code" to see the output on the right.')
+if run_code and code.strip() != "":
+    output, error = execute_python_code(code)
+    with col2:
+        output_placeholder.code(output, language='python')
+        if error:
+            st.error('Error in execution. Check the output for details.')
